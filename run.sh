2ab9b22f08a338785d0fd9a62a5f2d89386cdef7
Jakub Mrówczyński <jakub@mrowczynski.net>
2023-04-24 19:00:27 +0200

    wait for containers to stop in run script

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 if [ "$(sudo docker ps -q)" ]; then
     echo "Stopping running containers..."
     sudo docker stop $(sudo docker ps -q)
+    
+    echo "Waiting for all containers to stop..."
+    sudo docker container wait $(sudo docker ps -aq)
 else
     echo "No running containers found"
 fi
@@ -14,8 +17,8 @@ else
     echo "No existing images found"
 fi
 
-echo "Building container"
+echo "Building container..."
 sudo docker build -t express-ts -f docker/Dockerfile .
 
-echo "Running container"
+echo "Running container..."
 sudo docker run -d -p 80:8080 express-ts
